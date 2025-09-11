b412cc28d0f80f4390b7212b9e5d4edce7fa7b3a
lucalvex <lucalvex25@gmail.com>
2025-09-11 11:32:40 -0300

    commit

--- src/components/forms/RegisterForm.tsx
+++ src/components/forms/RegisterForm.tsx
@@ -64,7 +64,7 @@ export default function RegisterForm() {
 			options: [
 				{ label: 'Comércio', value: 'COMERCIO' },
 				{ label: 'Indústria', value: 'INDUSTRIA' },
-				{ label: 'Serviço', value: 'SERVICIO' },
+				{ label: 'Serviços', value: 'SERVICO' },
 			],
 		},
 		{
